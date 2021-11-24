ada3e3c85ad70a2b82b401fcf7fdfc3134cbaafc
amirmohamed <72021520+Amir94Mohamed@users.noreply.github.com>
2021-11-24 17:00:21 +0000

    Starting Logbook 2

---
+++ ASP-MVC-Movies/ASP-MVC-Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ASP_MVC_Movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
