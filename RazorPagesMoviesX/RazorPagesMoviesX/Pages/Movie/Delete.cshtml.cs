e0facb04cb27aed8b064fa4e01f31b3751a72183
amirmohamed <72021520+Amir94Mohamed@users.noreply.github.com>
2021-11-25 15:52:26 +0000

    Razor Pages Movies

---
+++ RazorPagesMoviesX/RazorPagesMoviesX/Pages/Movie/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMoviesX.Data;
+using RazorPagesMoviesX.Models;
+
+namespace RazorPagesMoviesX.Pages.Movie
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorPagesMoviesX.Data.RazorPagesMoviesXContext _context;
+
+        public DeleteModel(RazorPagesMoviesX.Data.RazorPagesMoviesXContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FindAsync(id);
+
+            if (Movie != null)
+            {
+                _context.Movie.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
