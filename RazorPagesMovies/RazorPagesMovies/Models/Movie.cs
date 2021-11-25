7d61a6b1e6fd06e405e4be78e854d34411f887a7
amirmohamed <72021520+Amir94Mohamed@users.noreply.github.com>
2021-11-25 21:52:54 +0000

    update

--- RazorPagesMovies/RazorPagesMovies/Models/Movie.cs
+++ RazorPagesMovies/RazorPagesMovies/Models/Movie.cs
@@ -5,12 +5,22 @@ namespace RazorPagesMovies.Models
 {
     public class Movie
     {
+        public enum Genres
+        {
+            Comedy, Drama, Horror, Thriller, Action, Fantasy
+        }
+        /// <summary>
+        /// Movie list/Database
+        /// </summary>
+        [Key]
         public int ID { get; set; }
+
+        [StringLength(60)]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+        public Genres Genre { get; set; }
         public decimal Price { get; set; }
     }
 }
