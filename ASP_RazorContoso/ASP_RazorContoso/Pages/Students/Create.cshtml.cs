cea76cdc5e6d5f8820842363913a9d9ea560adcb
amirmohamed <72021520+Amir94Mohamed@users.noreply.github.com>
2021-12-03 01:58:03 +0000

    Razor Contoso

---
+++ ASP_RazorContoso/ASP_RazorContoso/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ASP_RazorContoso.Data;
+using ASP_RazorContoso.Models;
+
+namespace ASP_RazorContoso.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ASP_RazorContoso.Data.SchoolContext _context;
+
+        public CreateModel(ASP_RazorContoso.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
